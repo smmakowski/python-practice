38528f165d5e2ead5ba312ff70e374580bee30b6
smmakowski <makowski.sm@gmail.com>
2018-03-03 15:26:21 -0800

    helloPython with print() and str()

---
+++ helloPython.py
@@ -0,0 +1,13 @@
+# print hello!
+
+print('Hello Python!')
+
+# calculate minutes in a week
+
+minutes_in_hour = 60
+hours_in_day = 24
+days_in_week = 7
+total_weeks = 7
+
+print('There are ' + str(minutes_in_hour * hours_in_day * days_in_week * total_weeks) + ' minutes in 7 weeks.') 
+
